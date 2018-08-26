8f70ed6b87dfacad5c1da3bd4c27930a869e6304
Ashish Singh <ashishcodes4@gmail.com>
2018-08-27 03:50:56 +0530

    bug fix for deploy

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post('/register', (req, res) => {
       })
       .then(trx.commit)
       .catch(trx.rollback);
-  }).catch(err => res.status(400).json('unable to register'));
+  }).catch(err => res.status(400).json('server side error while registering'));
 });
 
 //@DESC: '/profile/:id'
